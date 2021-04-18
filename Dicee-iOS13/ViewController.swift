51a29812a8bfb369f836c871620d2d43cbec5310
Amirali <courieriam@gmail.com>
2021-04-18 12:28:15 -0400

    Change dice on program load
    - Dice will now become transparent and show 6 when program loads

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -5,14 +5,23 @@
 //  Created by Angela Yu on 11/06/2019.
 //  Copyright © 2019 London App Brewery. All rights reserved.
 //
+//  Completed by Amir Kazeminejad
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageDiceOne: UIImageView!
+    @IBOutlet weak var imageDiceTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Change dices to be semi-transparent and show 6
+        imageDiceOne.image = #imageLiteral(resourceName: "DiceSix")
+        imageDiceOne.alpha = 0.5
+        imageDiceTwo.image = #imageLiteral(resourceName: "DiceSix")
+        imageDiceTwo.alpha = 0.5
     }
 
 
